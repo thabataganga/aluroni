281b29aa74c4366fc947c38450d37c7687b64911
thabataganga <ganga.thabata@gmail.com>
2022-07-03 23:50:21 -0300

    botao

---
+++ src/components/Botao/index.tsx
@@ -0,0 +1,5 @@
+import styles from "./Botao.module.scss";
+
+export default function Botao() {
+  return <button className={styles.botao}>Botão</button>;
+}
---
+++ src/pages/Cardapio/index.tsx
@@ -0,0 +1,11 @@
+import Botao from "../../components/Botao";
+import styles from "./Cardapio.module.scss";
+
+export default function Cardapio() {
+  return (
+    <main>
+      <nav className="menu">Cardapio</nav>
+      <Botao />
+    </main>
+  );
+}
