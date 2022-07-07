066dd1afd4f078c987f90f507ef08bb2015dca70
thabataganga <ganga.thabata@gmail.com>
2022-07-07 18:13:58 -0300

    not found

--- src/routes.js
+++ src/routes.js
@@ -5,6 +5,7 @@ import Menu from "components/Menu";
 import PaginaPadrao from "components/PaginaPadrao";
 import Sobre from "pages/Sobre";
 import Footer from "components/Footer";
+import NotFound from "pages/NotFound";
 
 export default function AppRouter() {
   return (
@@ -17,6 +18,7 @@ export default function AppRouter() {
             <Route path="cardapio" element={<Cardapio />} />
             <Route path="sobre" element={<Sobre />} />
           </Route>
+          <Route path="*" element={<NotFound />} />
         </Routes>
         <Footer />
       </Router>
