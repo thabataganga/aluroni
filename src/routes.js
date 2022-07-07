216348d52aac022cefbd50c2a285d2b3782d6434
thabataganga <ganga.thabata@gmail.com>
2022-07-07 11:41:02 -0300

    rotas

---
+++ src/routes.js
@@ -0,0 +1,14 @@
+import Inicio from "pages/Inicio";
+import Cardapio from "pages/Cardapio";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
+export default function AppRouter() {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Inicio />} />
+        <Route path="/cardapio" element={<Cardapio />} />
+      </Routes>
+    </Router>
+  );
+}
